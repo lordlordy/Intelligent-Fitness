6bdd5b3d6ffb85d79e7a429a510872c0919005e4
Steven Thomas Lord <stevenlord@me.com>
2019-06-14 11:48:04 +0100

    Initial Commit

---
+++ Intelligent Fitness/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Intelligent Fitness
+//
+//  Created by Steven Lord on 14/06/2019.
+//  Copyright © 2019 Steven Lord. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Intelligent Fitness/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Intelligent Fitness
+//
+//  Created by Steven Lord on 14/06/2019.
+//  Copyright © 2019 Steven Lord. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
